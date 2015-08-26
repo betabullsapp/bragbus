a6fbf0ff62c80c750977e185e0a0fa5052bb8ad8
Michael D. Mooring <mikeumus@gmail.com>
2015-08-26 03:07:36 +0000

    Mikes stylez.

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,45 +30,14 @@ jQuery(document).ready(function($) {
     $('input, textarea').placeholder();
     
     /* ======= jQuery FitVids - Responsive Video ======= */
-    $("#video-container").fitVids();    
+    //$("#video-container").fitVids();    
     
     /* ======= jQuery Responsive equal heights plugin ======= */
     /* Ref: https://github.com/liabru/jquery-match-height */
     
     $('#testimonials .quote-box').matchHeight(); 
-    
-    /* ======= Style Switcher (Remove on production site) ======= */
-    $('#config-trigger').on('click', function(e) {
-        var $panel = $('#config-panel');
-        var panelVisible = $('#config-panel').is(':visible');
-        if (panelVisible) {
-            $panel.hide();
-        } else {
-            $panel.show();
-        }
-        e.preventDefault();
-    });
-    
-    $('#config-close').on('click', function(e) {
-        e.preventDefault();
-        $('#config-panel').hide();
-    });
-    
-    
-    $('#color-options a').on('click', function(e) {
-        var $styleSheet = $(this).attr('data-style');
-        var $logoImage = $(this).attr('data-logo');
-		$('#theme-style').attr('href', $styleSheet);
-		$('#logo-image').attr('src', $logoImage);
-				
-		var $listItem = $(this).closest('li');
-		$listItem.addClass('active');
-		$listItem.siblings().removeClass('active');
-		
-		e.preventDefault();
-		
-	});
 	
-
+    $('.jotform-form').css('padding-right','57px');
+    $('.jotform-form').css('background-color','#FFFFFC');
 
 });
